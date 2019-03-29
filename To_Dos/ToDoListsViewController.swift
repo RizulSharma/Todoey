039eb6fb0d383466b1869266fb0d3713af38be69
Rizul Sharma <Rizulsharma16@gmail.com>
2019-03-29 13:26:29 +0530

    To Do list complete with data store and delegate methods.

---
+++ To_Dos/ToDoListsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  To_Dos
+//
+//  Created by Rizul Sharma on 28/03/19.
+//  Copyright © 2019 Rizul Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["find mike","buy eggos","demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        
+    }
+    
+    
+
+}
+
